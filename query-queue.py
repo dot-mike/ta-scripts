f0c4b2e916f959bd477744a050cec49103d8bd3d
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-03-11 14:36:55 +0100

    Fix query queue script. Handle pending Now properly shows position in queue.

--- query-queue.py
+++ query-queue.py
@@ -18,25 +18,36 @@ es = Elasticsearch(
 )
 
 
-def fetch_ids(es, index, scroll="2m", size=1000):
-    query = {"query": {"match_all": {}}}
-    data = helpers.scan(
-        es,
-        index=index,
-        query=query,
-        scroll=scroll,
-        size=size,
-        _source_includes=["youtube_id"],
-    )
-    for hit in data:
-        if "youtube_id" in hit["_source"]:
-            yield hit["_source"]["youtube_id"]
-
-
 def main():
     print("Fetching video ids from download queue")
     download_index = "ta_download"
-    queued_videos = set(fetch_ids(es, download_index))
+
+    ignored = False
+    if ignored:
+        filter_view = "ignore"
+    else:
+        filter_view = "pending"
+
+    must_list = [{"term": {"status": {"value": filter_view}}}]
+
+    query = {
+        "query": {"bool": {"must": must_list}},
+        "sort": [
+            {"auto_start": {"order": "desc"}},
+            {"timestamp": {"order": "asc"}},
+        ],
+    }
+    iterator = helpers.scan(
+        es,
+        index=download_index,
+        query=query,
+        scroll="5m",
+        preserve_order=True,
+    )
+    queued_videos = []
+    for video in iterator:
+        queued_videos.extend([video["_source"]["youtube_id"]])
+
     print(f"Found {len(queued_videos)} video ids in download queue")
 
     if len(sys.argv) < 2:
@@ -54,7 +65,14 @@ def main():
                 video_id = query["v"][0]
 
     if video_id in queued_videos:
-        print(f"Video {video_id} is in the download queue")
+        index = queued_videos.index(video_id)
+        print(
+            f"Video {video_id} is in the download queue at index {index} out of {len(queued_videos)}"
+        )
+        query = {"query": {"match": {"youtube_id": video_id}}}
+        results = es.search(index=download_index, body=query)
+        print(results["hits"]["hits"][0])
+
     else:
         print(f"Video {video_id} is not in the download queue")
 
