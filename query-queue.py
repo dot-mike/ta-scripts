3849585759e42e123fe7d43982876a72a4b37604
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-03-13 19:16:57 +0100

    Improve query queue to accept file with ids

--- query-queue.py
+++ query-queue.py
@@ -1,14 +1,19 @@
 """
-This script will check if a video is in the download queue
+This script will check if a video is in the download queue.
 
 Usage:
-    python query-queue.py <video_id>
+    python query-queue.py [<video_id> | <input_file>]
+    Specify either:
+    <video_id> - A single video ID or the youtube URL
+    <input_file> - File containing list of video IDs
 """
 
 import os
 import sys
 import pickle
 import time
+import re
+from pathlib import Path
 from urllib.parse import urlparse, parse_qs
 from elasticsearch import Elasticsearch, helpers
 from dotenv import load_dotenv
@@ -20,59 +25,75 @@ es = Elasticsearch(
     [os.getenv("ES_HOST")], basic_auth=(os.getenv("ES_USER"), os.getenv("ES_PASSWORD"))
 )
 
+script_dir = os.path.dirname(__file__)
 
-def main():
-    print("Starting script")
-    download_index = "ta_download"
 
-    ignored = False
-    if ignored:
-        filter_view = "ignore"
-    else:
-        filter_view = "pending"
+class VideoChecker:
+    """Class to check if a video is in the download queue"""
 
-    must_list = [{"term": {"status": {"value": filter_view}}}]
+    def __init__(self, client):
+        self.queued_videos = []
+        self.client = client
+        self.cache_file = Path(script_dir, "queued_videos.pkl")
 
-    query = {
-        "query": {"bool": {"must": must_list}},
-        "sort": [
-            {"auto_start": {"order": "desc"}},
-            {"timestamp": {"order": "asc"}},
-        ],
-    }
+        self.fetch_queued_videos()
 
-    cache_file = "queued_videos.pkl"
-    queued_videos = []
+    def fetch_queued_videos(self):
+        """Fetches video ids from the download queue"""
+        if os.path.exists(self.cache_file):
+            cache_time = os.path.getmtime(self.cache_file)
+            if (time.time() - cache_time) / 60 < 30:  # 30 minutes
+                print("Using ta_download video ids from cache")
+                with open(self.cache_file, "rb") as f:
+                    self.queued_videos = pickle.load(f)
 
-    if os.path.exists(cache_file):
-        cache_time = os.path.getmtime(cache_file)
-        if (time.time() - cache_time) / 60 < 30:
-            print("Fetching video ids from cache")
-            with open(cache_file, "rb") as f:
-                queued_videos = pickle.load(f)
+            return self.queued_videos
 
-    if not queued_videos:
-        print("Fetching video ids in download queue from Elasticsearch")
+        print("Fetching ta_download video ids from Elasticsearch")
+
+        ignored = False
+        if ignored:
+            filter_view = "ignore"
+        else:
+            filter_view = "pending"
+
+        must_list = [{"term": {"status": {"value": filter_view}}}]
+
+        query = {
+            "query": {"bool": {"must": must_list}},
+            "sort": [
+                {"auto_start": {"order": "desc"}},
+                {"timestamp": {"order": "asc"}},
+            ],
+        }
+
         iterator = helpers.scan(
-            es,
-            index=download_index,
+            self.client,
+            index="ta_download",
             query=query,
             scroll="5m",
             preserve_order=True,
         )
         for video in iterator:
-            queued_videos.extend([video["_source"]["youtube_id"]])
+            self.queued_videos.extend([video["_source"]["youtube_id"]])
 
-        with open(cache_file, "wb") as f:
-            pickle.dump(queued_videos, f)
+        with open(self.cache_file, "wb") as f:
+            print(
+                f"Saving {len(self.queued_videos)} video ids to cache: {self.cache_file}"
+            )
+            pickle.dump(self.queued_videos, f)
 
-    print(f"Found {len(queued_videos)} video ids in download queue")
+        return self.queued_videos
 
-    if len(sys.argv) < 2:
-        video_id = input("Enter video id: ")
-    else:
-        video_id = sys.argv[1]
+    def check_video_id(self, video_id):
+        """Check if video is in the download queue"""
 
+        if video_id in self.queued_videos:
+            return self.queued_videos.index(video_id)
+
+
+def extract_video_id(video_id=None):
+    """Extracts video ID from the string"""
     if "youtube.com" in video_id or "youtu.be" in video_id:
         url_data = urlparse(video_id)
         query = parse_qs(url_data.query)
@@ -81,18 +102,47 @@ def main():
         else:
             if "v" in query:
                 video_id = query["v"][0]
+    else:
+        id_search = re.search(r"([a-zA-Z0-9_-]{11})", video_id)
+        if id_search is not None:
+            video_id = id_search.group(1)
 
-    if video_id in queued_videos:
-        index = queued_videos.index(video_id)
-        print(
-            f"Video {video_id} is in the download queue at index {index} out of {len(queued_videos)}"
-        )
-        query = {"query": {"match": {"youtube_id": video_id}}}
-        results = es.search(index=download_index, body=query)
-        print(results["hits"]["hits"][0])
+    return video_id
+
+
+def main():
+    """Main function"""
+
+    print("Starting script")
+
+    filename = os.path.basename(__file__)
+    if len(sys.argv) < 2:
+        print(f"Usage: python {filename} [<video_id>|<input_file>]")
+        return
+
+    user_input = sys.argv[1]
+
+    if os.path.exists(user_input):
+        with open(user_input, "r", encoding="utf-8") as f:
+            video_ids = f.readlines()
+            video_ids = [extract_video_id(video_id) for video_id in video_ids]
 
     else:
-        print(f"Video {video_id} is not in the download queue")
+        video_ids = [extract_video_id(user_input)]
+
+    if not video_ids:
+        print("No video IDs found")
+        return
+
+    checker = VideoChecker(es)
+    for video_id in video_ids:
+        result = checker.check_video_id(video_id)
+        if result is not None:
+            print(f"Video {video_id} is in the download queue at position {result}")
+        else:
+            print(f"Video {video_id} is not in the download queue")
+
+    print("Script completed successfully!")
 
 
 if __name__ == "__main__":
