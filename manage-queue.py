53069c70b752ed57eba0e4528d7ad1b761c6fafe
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-03-14 21:37:24 +0100

    refactor: use click for arg parsing in manage-queue.py

--- manage-queue.py
+++ manage-queue.py
@@ -1,26 +1,16 @@
 """
-This script will let you add or remove videos from the download queue
-using the TubeArchivist API.
-
-Commands:
-    add - Add video to the download queue
-    remove - Remove video from the download queue
-    priority - Move video to priority
-    query - Query video status in the download queue
-
-Usage:
-    python {filename} COMMAND ARG1
-        - COMMAND: Either "add" or "remove"
-        - ARG1: Video ID or file containing video IDs
+This script will let you manage the download queue of TubeArchivist.
 """
 
 import os
 import sys
 import re
+import time
 import requests
+import click
+from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
 from urllib.parse import urlparse, parse_qs
 from dotenv import load_dotenv
-import time
 
 
 load_dotenv()
@@ -33,131 +23,54 @@ TA_URL = os.getenv("TA_URL")
 API_URL = f"{TA_URL}/api"
 
 
+def process_input_data(input_data):
+    # Determine if input_data is a file or a list of video IDs/URLs
+    if len(input_data) == 1 and os.path.exists(input_data[0]):
+        with open(input_data[0], "r", encoding="utf-8") as f:
+            video_ids = [
+                vid
+                for vid in (extract_video_id(line.strip()) for line in f)
+                if vid is not None
+            ]
+    else:
+        video_ids = [extract_video_id(video_str) for video_str in input_data]
+
+    if not video_ids or not any(video_ids):
+        print("No valid video IDs provided")
+        sys.exit(1)
+
+    return video_ids
+
+
 def video_exists(video_id):
     r = session.get(f"{API_URL}/video/{video_id}/")
     return r.status_code == 200
 
 
-def extract_video_id(video_id=None):
-    """Extracts video ID from the string"""
-    if "youtube.com" in video_id or "youtu.be" in video_id:
-        url_data = urlparse(video_id)
-        query = parse_qs(url_data.query)
+def extract_video_id(video_str):
+    """
+    Extracts video ID from the provided text input or URL.
+    """
+
+    video_str = video_str.strip()
+
+    if re.fullmatch(r"[a-zA-Z0-9_-]{11}", video_str):
+        return video_str
+
+    if "youtube.com" in video_str or "youtu.be" in video_str:
+        url_data = urlparse(video_str)
         if url_data.netloc == "youtu.be":
-            video_id = url_data.path[1:]
+            return url_data.path[1:]
         else:
-            if "v" in query:
-                video_id = query["v"][0]
-    else:
-        id_search = re.search(r"([a-zA-Z0-9_-]{11})", video_id)
-        if id_search is not None:
-            video_id = id_search.group(1)
+            url_query = parse_qs(url_data.query)
+            return url_query.get("v", [None])[0]
 
-    return video_id
+    return None
 
 
-def main(input_data, user_command):
-    """Main function"""
-
-    if os.path.exists(input_data):
-        with open(input_data, "r", encoding="utf-8") as f:
-            video_ids = f.readlines()
-            video_ids = [extract_video_id(video_id) for video_id in video_ids]
-
-    else:
-        video_ids = [extract_video_id(input_data)]
-
-    if not video_ids:
-        print("No video IDs found in the input specified")
-        return
-
-    if user_command == "add":
-        unique_video_ids = [
-            video_id for video_id in video_ids if not video_exists(video_id)
-        ]
-        if not unique_video_ids:
-            print("All videos already exist in the database")
-            return
-
-        data = {
-            "data": [
-                {
-                    "youtube_id": "https://www.youtube.com/watch?v=" + video_id,
-                    "status": "pending",
-                }
-                for video_id in unique_video_ids
-            ]
-        }
-        r = session.post(f"{API_URL}/download/?autostart=true", json=data)
-        r.raise_for_status()
-        if r.status_code == 200:
-            print(f"Added {len(video_ids)} videos to the download queue")
-        print("Response:")
-        print(r.json())
-
-        print(
-            "Videos will be extracted and added to the download queue shortly.\n"
-            "This can take a some minutes depending on the number of videos."
-        )
-
-    if user_command == "remove":
-        for video_id in video_ids:
-            print(f"Removing video {video_id} from the download queue")
-            r = session.delete(f"{API_URL}/download/{video_id}/")
-            if r.status_code != 200:
-                print(f"Failed to remove video {video_id} from the download queue")
-            time.sleep(2)
-
-    if user_command == "priority":
-        for video_id in video_ids:
-            print(f"Moving video {video_id} to priority")
-            r = session.post(
-                f"{API_URL}/download/{video_id}/", json={"status": "priority"}
-            )
-            if r.status_code != 200:
-                print(f"Failed to move video {video_id} to priority")
-            print(r)
-            time.sleep(2)
-
-    if user_command == "query":
-        for video_id in video_ids:
-            # first check if the video already exist
-
-            r = session.get(f"{API_URL}/download/{video_id}/")
-            if r.status_code == 404:
-                print(f"Video {video_id} not found in the download queue")
-                continue
-            if r.status_code != 200:
-                print(f"Failed to query video {video_id} in the download queue")
-                continue
-            else:
-                # print(r.json())
-                print(f"Video {video_id} is in the download queue")
-            # first check if video is refreshed
-            # r = session.get(f"{API_URL}/refresh/?type=video&id={video_id}")
-            # if r.status_code != 200:
-            #    print(f"Failed to query video {video_id}")
-            #    continue
-
-            ## {'total_queued': 593, 'type': 'video', 'id': 'uE-1RPDqJAY', 'state': False}
-
-            # data = r.json()
-            # if data["state"] == False:
-            #    print(f"Video {video_id} is waiting to be refreshed")
-            #    # start refresh
-            #    r = session.post(f"{API_URL}/refresh/", json={"video": [video_id]})
-            #    print(r)
-            #    print(r.json())
-
-            # print(r.json())
-
-    print("Script completed successfully!")
-
-
-if __name__ == "__main__":
-    filename = os.path.basename(__file__)
-    docstring = __doc__.format(filename=filename)
-
+@click.group()
+def cli():
+    """This script lets you add or remove videos from the download queue using the TubeArchivist API."""
     session.headers.update(
         {
             "Authorization": f"Token {os.getenv('TA_API_TOKEN')}",
@@ -173,29 +86,124 @@ if __name__ == "__main__":
         print("Please set all required environment variables. See .env.sample")
         sys.exit(1)
 
-    if len(sys.argv) < 2:
-        print(docstring)
-        sys.exit(0)
 
-    if len(sys.argv) < 3:
-        if sys.argv[1] == "-h":
-            print(docstring)
-            sys.exit(0)
+@cli.command()
+@click.argument("input_data", nargs=-1, type=str)
+def add(input_data):
+    if not input_data:
+        print("No video IDs provided")
+        return
 
-        print("Not enough arguments provided.")
-        print("Run with -h for help")
-        sys.exit(1)
+    video_ids = process_input_data(input_data)
 
-    command = sys.argv[1]
-    user_input = sys.argv[2]
+    unique_video_ids = [
+        video_id for video_id in video_ids if not video_exists(video_id)
+    ]
+    if not unique_video_ids:
+        print("All videos already exist in the database")
+        return
 
-    if any([command == "-h", user_input == "-h"]):
-        print(docstring)
-        sys.exit(0)
+    data = {
+        "data": [
+            {
+                "youtube_id": "https://www.youtube.com/watch?v=" + video_id,
+                "status": "pending",
+            }
+            for video_id in unique_video_ids
+        ]
+    }
+    r = session.post(f"{API_URL}/download/?autostart=true", json=data)
+    r.raise_for_status()
+    if r.status_code == 200:
+        print(f"Added {len(video_ids)} videos to the download queue")
+    print("Response:")
+    print(r.json())
 
-    if command not in ["add", "remove", "priority", "query"]:
-        print(f"Invalid command: {command}")
-        print("Run with -h for help")
-        sys.exit(1)
+    print(
+        "Videos will be extracted and added to the download queue shortly.\n"
+        "This can take a some minutes depending on the number of videos."
+    )
 
-    main(user_input, command)
+
+@cli.command()
+@click.argument("input_data", nargs=-1, type=str)
+def priority(input_data):
+    video_ids = process_input_data(input_data)
+    if not video_ids:
+        return
+
+    for video_id in video_ids:
+        print(f"Moving video {video_id} to priority")
+        r = session.post(f"{API_URL}/download/{video_id}/", json={"status": "priority"})
+        if r.status_code != 200:
+            print(f"Failed to move video {video_id} to priority")
+        print(r)
+        time.sleep(2)
+
+
+@cli.command()
+@click.argument("input_data", nargs=-1, type=str)
+@click.option(
+    "--only-missing",
+    "-m",
+    is_flag=True,
+    help="Only print the video IDs that are not found",
+)
+@optgroup.group(
+    "Query option",
+    cls=RequiredMutuallyExclusiveOptionGroup,
+    help="Choose if you want to query the download queue(default) or the index.",
+)
+@optgroup.option(
+    "--download-queue",
+    "-d",
+    is_flag=True,
+    help="Query the video status in the download queue.",
+)
+@optgroup.option(
+    "--index", "-i", is_flag=True, help="Query the video status in the index."
+)
+def query(input_data, only_missing, download_queue, index):
+    video_ids = process_input_data(input_data)
+    if not video_ids:
+        return
+
+    if download_queue:
+        url = f"{API_URL}/download"
+        query_type = "queue"
+
+    if index:
+        url = f"{API_URL}/video"
+        query_type = "index"
+
+    missing_videos = []
+    found_videos = []
+    failed_videos = []
+
+    for video_id in video_ids:
+        r = session.get(f"{url}/{video_id}/")
+        if r.status_code == 404:
+            missing_videos.append(video_id)
+            continue
+        if r.status_code != 200:
+            failed_videos.append(video_id)
+            continue
+        else:
+            found_videos.append(video_id)
+
+    if only_missing:
+        print("Missing videos:")
+        print("\n".join(missing_videos))
+    else:
+        print(f"Found videos in the {query_type}:")
+        print("\n".join(found_videos))
+        print(f"Missing videos in the {query_type}:")
+        print("\n".join(missing_videos))
+        print(f"Failed videos in the {query_type}:")
+        print("\n".join(failed_videos))
+
+
+if __name__ == "__main__":
+    filename = os.path.basename(__file__)
+    docstring = __doc__.format(filename=filename)
+    cli()
